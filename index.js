ab1b0181e96b159c5df426b9c17a6532684c4cc0
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-26 16:44:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+let cards = []
+
+function writeCards(array, event){
+    for (let i = 0; i < array.length; i++)
+    cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    return cards
+}
+
+function countDown(num){
+    for (let i = num; i >= 0; i--)
+    console.log(i)
+}
+
+
